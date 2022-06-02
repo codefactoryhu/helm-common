4d9d363c6860c04391770e03b6a593354bad8f0a
Zoltan Vigh <vigh.zoltan@codefactory.hu>
2022-06-02 21:37:04 +0200

    deleted unnecessary directory and files

--- chart-test/run-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-rm charts/helm-common-*
-helm package ../
-mkdir -p charts && mv -v helm-common-* charts/
-go mod download
-gotestsum --format pkgname-and-test-fails
--- docs/docs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-helm-docs --template-files=./docs/README.md.gotmpl --template-files=./docs/_templates.gotmpl --chart-search-root=..
